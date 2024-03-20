6768b324469cedf26791ff52b976e4cf09c964d1
QixinDeng <qxdeng1991@gmail.com>
2024-03-20 10:44:49 -0400

    Add files via upload

---
+++ include/os-examples.h
@@ -0,0 +1,53 @@
+/*
+    Name: Dr. Qixin Deng
+    Date: February 28, 2024
+    Description:
+    Header file.
+
+    Contact Information:
+    - Email: dengq@wabash.edu
+    - GitHub: github.com/QixinDeng
+
+    MIT License
+
+    Copyright (c) 2024 Your Name
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+*/
+
+#ifndef CODE_OS_EXAMPLES_H
+#define CODE_OS_EXAMPLES_H
+int forkExample();
+int nestedFork();
+int unitOfExecution();
+int fasterThread();
+int splitAJob();
+int mutualExclusion();
+int semExample();
+int monitorExample();
+int messagePassing();
+int pipeExample();
+int mutexExample();
+int semRWExample();
+int monitorRW();
+int bankers();
+int detection();
+int philosopherSem();
+int philosopherMon();
+#endif //CODE_OS_EXAMPLES_H
